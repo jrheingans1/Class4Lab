c6d4ec00d1adf41e18cee9250b3b458da5b1c3b5
jrheingans1 <jrheingans1@L11713038597.wctc.msft>
2013-01-31 17:10:37 -0600

    Lab2 deleted unneeded Methods and fixed syntax errors. Also added a main Method

--- src/lab2/IntroJavaCourse.java
+++ src/lab2/IntroJavaCourse.java
@@ -6,33 +6,15 @@ package lab2;
  * @author      your name goes here
  * @version     1.00
  */
-public class IntroJavaCourse {
-    String courseName;
-    private String courseNumber;
-    private double credits;
+public class IntroJavaCourse extends ProgrammingCourses {
+   
     private String prerequisites;
 
     public IntroJavaCourse(String courseName, String courseNumber) {
-        this.courseName = courseName;
-        this.courseNumber = courseNumber;
+        setCourseName(courseName);
+        setCourseNumber(courseNumber);
     }
-
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
-
-    public double getCredits() {
-        return credits;
-    }
-
-    public void setCredits(double credits) {
-        this.credits = credits;
-    }
-
+   
     public String getPrerequisites() {
         return prerequisites;
     }
